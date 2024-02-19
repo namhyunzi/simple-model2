f0569b2a90d7c592395895605dfd91adea902037
namhyunzi <jhta@DESKTOP-LU4J5LN>
2024-02-19 17:51:19 +0900

    24-02-19

--- src/main/java/dao/BoardDao.java
+++ src/main/java/dao/BoardDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 
@@ -12,17 +13,21 @@ public class BoardDao {
 
 	private final SqlMapClient ibatis = IbatisUtil.getSqlMapClient();
 	
+	public int getTotalRows() throws SQLException {
+		return (int) ibatis.queryForObject("boards.getTotalRows");
+	}
+	
 	@SuppressWarnings("unchecked")
-	public List<Board> getAllBoards() throws SQLException {
-		return (List<Board>) ibatis.queryForList("boards.getAllBoards");
+	public List<Board> getAllBoards(Map<String, Object> param) throws SQLException {
+		return (List<Board>) ibatis.queryForList("boards.getAllBoards", param);
 	}
 	public Board getBoardByNo(int no) throws SQLException {
 		return (Board) ibatis.queryForObject("boards.getBoardByNo", no);
 	}
-	public void insertBoards (Board board) throws SQLException {
-		ibatis.insert("boards.insertBoards", board);
+	public void insertBoard (Board board) throws SQLException {
+		ibatis.insert("boards.insertBoard", board);
 	}
-	public void updateBoards (Board board) throws SQLException {
-		ibatis.update("boards.updateBoards", board);
+	public void updateBoard (Board board) throws SQLException {
+		ibatis.update("boards.updateBoard", board);
 	}
 }
